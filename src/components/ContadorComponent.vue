225ba3e2ac45526eb32b410d19bbb059eae64e82
amazing79 <ignacioja@hotmail.com>
2025-06-06 15:18:54 -0300

    Limpieza de archivos inncesarios y creacion de una lista de tareas simple

---
+++ src/components/ContadorComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+  import { ref } from 'vue'
+  const contador = ref(0);
+
+  function incrementar(){
+    contador.value++;
+  }
+
+  function decrementar(){
+    contador.value--;
+  }
+
+</script>
+<template>
+  <div>
+    <p>Valor actual: {{ contador }}</p>
+    <button @click="incrementar">Add</button>
+    <button @click="decrementar">Sub</button>
+  </div>
+</template>
---
+++ src/components/ListaDeTareas.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import {ref} from 'vue';
+
+  const tareas = ref([]);
+  const newTask = ref('');
+
+  const addTask = () => {
+    tareas.value.push(newTask.value);
+    newTask.value = '';
+  }
+
+</script>
+
+<template>
+  <div>
+    <h2>Lista de tareas</h2>
+    <input type="text" v-model="newTask" @keyup.enter="addTask">
+    <ul>
+      <li v-for="(tarea, idx) in tareas" :key="idx">
+        {{tarea}}</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+</style>
