b0d17a38e21ef314b353fdde3926c404f31d7a34
amazing79 <ignacioja@hotmail.com>
2025-06-07 18:24:07 -0300

    Actualización del ejemplo de Pinia pero sin usar watchers

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,26 +1,15 @@
 <script setup>
-  import { ref, watch } from 'vue';
   import { useRegistrarStore } from '@/modules/registro/stores/registrarStore';
 
   const registrarStore = useRegistrarStore();
-  const nombre = ref(registrarStore.nombre.value);
-  const email = ref(registrarStore.email.value);
-
-  watch(() => registrarStore.nombre, (newValue) => {
-    nombre.value = newValue;
-  });
-
-  watch(() => registrarStore.email, (newValue) => {
-    email.value = newValue;
-  });
 
 </script>
 
 <template>
   <div class="sidebar">
     <h2>Bienvenido</h2>
-    <h3>Nombre: {{ nombre }}</h3>
-    <h3>Email: {{ email }}</h3>
+    <h3>Nombre: {{ registrarStore.nombre }}</h3>
+    <h3>Email: {{ registrarStore.email }}</h3>
     <ul>
       <li>
         <router-link to="/contador">
@@ -38,14 +27,12 @@
         </router-link>
       </li>
     </ul>
-
-
   </div>
 </template>
 
 <style scoped>
   .sidebar{
-    width: 200px;
+    width: 300px;
     background-color: #f0f0f0;
     padding: 20px;
   }
